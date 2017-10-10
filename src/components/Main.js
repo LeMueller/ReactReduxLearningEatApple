84cc7721ab7c810a6c37e2cdaac4663a9d90b2af
lezahng <lezhang@techfak.uni-bielefeld.de>
2017-10-10 21:09:25 +0200

    redux lerning, action and reducer

---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+require('normalize.css/normalize.css');
+//require('styles/App.css');
+
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware  } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import reducer from '../reducers/appleBasketReducer.js';
+
+import AppleBasket from '../containers/AppleBasket.jsx'
+import '../styles/appleBasket.scss'
+import '../styles/appleitem.scss'
+
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      	<Provider store={store}>
+        	<AppleBasket />
+    	</Provider>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
